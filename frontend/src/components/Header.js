d869d061c1dad9d71b152d5ca16125dd6f28d825
Daniel Busch <dakopen185@gmail.com>
2024-04-09 23:40:44 +0200

    different header based on user role

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,58 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
+import {
+	isTeacher,
+	isSecretary,
+	isAdmin,
+	isStudyStudent,
+	isTeacherOrSecretary,
+	isTeacherOrAdmin,
+	isSecretaryOrAdmin,
+	isTeacherOrSecretaryOrAdmin,
+} from "../utils/RoleChecks";
 
 const Header = () => {
 	let { user, logoutUser } = useContext(AuthContext);
 	return (
 		<div>
-			<Link to="/">Home</Link>
+			{isStudyStudent(user) && (
+				<>
+					<Link to="/">Home</Link>
+				</>
+			)}
+			{isTeacher(user) && (
+				<>
+					<Link to="/">Home</Link>
+					<br></br>
+					<Link to="/courses">Kurse</Link>
+				</>
+			)}
+			{isSecretary(user) && (
+				<>
+					<Link to="/">Home</Link>
+					<br></br>
+					<Link to="/create-teacher">Lehreraccount erstellen</Link>
+					<br></br>
+					<Link to="/courses">Kurse</Link>
+				</>
+			)}
+			{isAdmin(user) && (
+				<>
+					<Link to="/">Home</Link>
+					<br></br>
+					<Link to="/create-teacher">Lehreraccount erstellen</Link>
+					<span> | </span>
+					<Link to="/create-any-role">Staff Account erstellen</Link>
+					<br></br>
+					<Link to="/courses">Kurse</Link>
+					<span> | </span>
+
+					<Link to="/schools">Schulen</Link>
+				</>
+			)}
+
 			<br></br>
-			<Link to="/create-teacher">Create Teacher</Link>
-			<span> | </span>
 			{user ? (
 				<p onClick={logoutUser}>Logout</p>
 			) : (
@@ -20,12 +63,6 @@ const Header = () => {
 				</>
 			)}
 			{user && <p>Hello user with Role: {user.role}</p>}
-			<br></br>
-			<Link to="/create-any-role">Create any role</Link>
-			<br></br>
-			<Link to="/schools">Schulen</Link>
-			<br></br>
-			<Link to="/courses">Kurse</Link>
 			<hr></hr>
 		</div>
 	);
